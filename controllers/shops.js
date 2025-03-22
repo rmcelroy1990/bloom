d37b950bc9f3fe4af1654f9befade95cf310c32e
rmcelroy1990 <mcelroy.raeme@gmail.com>
2025-03-22 15:39:35 -0500

    implemented EJS partials into web app and created nav bar component

--- controllers/shops.js
+++ controllers/shops.js
@@ -3,4 +3,13 @@ const router = express.Router();
 
 const User = require('../models/user.js');
 
+router.get('/', async (req, res) => {
+    try {
+        res.render('shops/index.ejs');
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
+    }
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -35,14 +35,16 @@ app.use(
 app.use(passUserToView);
 
 app.get('/', (req, res) => {
-  res.render('index.ejs', {
-    user: req.session.user,
-  });
+  if (req.session.user) {
+    res.redirect(`/users/${req.session.user._id}/shops`);
+  } else {
+    res.render('index.ejs');
+  }
 });
 
 app.use('/auth', authController);
 app.use(isSignedIn);
-app.use('/users/userId/shops', shopsController);
+app.use('/users/:userId/shops', shopsController);
 
 // app.get('/garden-lounge', (req, res) => {
 //   if (req.session.user) {
