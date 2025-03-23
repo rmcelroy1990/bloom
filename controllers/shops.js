0fdfd7b1d13f762fae0bde726d43319c852da101
rmcelroy1990 <mcelroy.raeme@gmail.com>
2025-03-23 13:27:28 -0500

    built show route and view

--- controllers/shops.js
+++ controllers/shops.js
@@ -5,7 +5,10 @@ const User = require('../models/user.js');
 
 router.get('/', async (req, res) => {
     try {
-        res.render('shops/index.ejs');
+        const currentUser = await User.findById(req.session.user._id);
+        res.render('shops/index.ejs', {
+            shops: currentUser.shops,
+        });
     } catch (error) {
         console.log(error);
         res.redirect('/');
@@ -19,7 +22,7 @@ router.get('/new', async (req, res) => {
 router.post('/', async (req, res) => {
     try {
       const currentUser = await User.findById(req.session.user._id);
-      currentUser.applications.push(req.body);
+      currentUser.shops.push(req.body);
       await currentUser.save();
       res.redirect(`/users/${currentUser._id}/shops`);
     } catch (error) {
@@ -27,5 +30,18 @@ router.post('/', async (req, res) => {
       res.redirect('/');
     }
   });
+
+router.get('/:shopId', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+        const shop = currentUser.shops.id(req.params.shopId);
+        res.render('shops/show.ejs', {
+            shop: shop,
+        });
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
+    }
+});
 
 module.exports = router;
